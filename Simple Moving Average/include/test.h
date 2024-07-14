3aa3014bbd5655dc4f6f2e14653f4cfae60ac878
Денис <denis26russia26@gmail.com>
2024-07-14 20:49:18 +0300

    feat: добавлена логика тестирования

---
+++ Simple Moving Average/include/test.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "include/simple_moving_average.h"
+#include <chrono>
+#include <iostream>
+
+template<typename RealType = double>
+void test(const std::list<size_t>& win_sizes, size_t seq_len) {
+	namespace chr = std::chrono;
+	auto rand_seq = alg::randn_seq<RealType>(0, 1, seq_len);
+	for (auto& win_size : win_sizes) {
+		auto begin_tp = chr::steady_clock::now();
+		auto result = alg::simple_moving_average(rand_seq, win_size);
+		auto diff = chr::duration_cast<chr::milliseconds>(chr::steady_clock::now() - begin_tp);
+		auto points_per_sec = (result.size() * 1000) / double(diff.count());
+		std::cout << "For averaging window of size " << win_size << ", performance is " << int(points_per_sec)  << " points per second;" << std::endl;
+
+	}
+}
--- Simple Moving Average/main.cpp
+++ Simple Moving Average/main.cpp
@@ -1,4 +1,23 @@
-﻿
+﻿#include "include/test.h"
+
+
 int main()
 {
+	enum class real_t {FLOAT, DOUBLE};
+	const size_t seq_len = 1000000;
+	const std::list<size_t> win_size = { 4, 8, 16, 32, 64, 128 };
+	const std::list<real_t> types = { real_t::FLOAT, real_t::DOUBLE };
+
+	for (auto& type : types) {
+		if (type == real_t::FLOAT) {
+			std::cout << "float type: " << std::endl;
+			test<float>(win_size, seq_len);
+		}
+		else {
+			std::cout << "double type: " << std::endl;
+			test(win_size, seq_len);
+		}
+	}
+
+	
 }
